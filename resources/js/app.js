7e8cc65799d316738ed58a13833a0ecd4acf88e1
douglasrono <douglasronno@gmail.com>
2023-08-13 22:26:46 +0300

    reverted changed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,1 @@
-import { createApp } from 'vue';
-
-import 'bootstrap-icons/font/bootstrap-icons.css'; // Import Bootstrap Icons CSS
-
-const app = createApp({});
-
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
-
-app.mount('#app');
+import './bootstrap';
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
 
-Auth::routes();
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/', [PageController::class, 'index'])->name('home');
-Route::get('about', [PageController::class, 'about'])->name('about');
-Route::get('skills', [PageController::class, 'skills'])->name('skills');
-Route::get('experience', [PageController::class, 'experience'])->name('experience');
-Route::get('portfolio', [PageController::class, 'portfolio'])->name('portfolio');
-Route::get('contact', [PageController::class, 'contact'])->name('contact');
+Route::get('/', function () {
+    return view('welcome');
+});
