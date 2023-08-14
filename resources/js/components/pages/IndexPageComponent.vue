e3e6ce8b251d0e690c547c29a31e89dfe0f1cb8b
douglasrono <douglasronno@gmail.com>
2023-08-14 23:31:04 +0300

    latest stable changes

---
+++ resources/js/components/pages/IndexPageComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+      <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero d-flex align-items-center">
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-6 d-flex flex-column justify-content-center">
+        <h5>Hello, I am</h5>
+      <h1 data-aos="fade-up">Douglas Rono</h1>
+      <h2 data-aos="fade-up" data-aos-delay="400">I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences.</h2>
+      <div data-aos="fade-up" data-aos-delay="600">
+        <div class="text-center text-lg-start">
+          <a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+            <span>Read more</span>
+            <i class="bi bi-arrow-right"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
+      <img src="assets/img/prof.png" class="img-fluid" alt="">
+    </div>
+  </div>
+</div>
+
+</section><!-- End Hero -->
+
+    </div>
+  </template>
+
+  <script>
+  export default {
+    data() {
+      return {
+
+      };
+    },
+    mounted() {
+
+    },
+    created() {
+
+    },
+    props: {
+
+    }
+  };
+  </script>
+
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<index-page-component></index-page-component>
+@endsection
