1b1ccdc95d25290850e001c686eb26c29edf846c
douglasrono <douglasronno@gmail.com>
2023-08-26 20:42:52 +0300

    initial changes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
-use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PagesController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/',[PagesController::class, 'index'])->name('index');
-Route::get('/blog',[BlogController::class,'index'])->name('blog.index');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
